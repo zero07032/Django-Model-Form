e9d78b102f9fd21bd036eac18a47c88e249b0051
zero07032 <overflow460@gmail.com>
2024-04-23 01:24:21 +0600

    Completed model form

--- form/urls.py
+++ form/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
-from .views import BlogList
+from .views import BlogList, BlogCreate
 
 urlpatterns = [
-    path("", BlogList, name="blog-list")
-]
+    path("", BlogList, name="blog-list"),
+    path("blog-create", BlogCreate, name="blog-create"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- form/views.py
+++ form/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .blog_form import BlogForm
+from .models import Blog
 
 
 def BlogList(request):
-    context = {}
+    context = {"blogs": Blog.objects.all()}
     return render(request, "home.html", context)
 
 
 def BlogCreate(request):
+    if request.method == "POST":
+        form = BlogForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your blog has been created!")
+        else:
+            messages.warning(request, "Something wrong!")
     return redirect("/")
