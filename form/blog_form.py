a82d27e3a23987bb4209f6563ed945158ce10dec
zero07032 <overflow460@gmail.com>
2024-04-23 21:55:16 +0600

    updated

--- form/blog_form.py
+++ form/blog_form.py
@@ -1,9 +1,10 @@
-from django.forms import ModelForm
+from django import forms
 
 from .models import Blog
+from django.core.exceptions import ValidationError
 
 
-class BlogForm(ModelForm):
+class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ["title", "description", "image"]
